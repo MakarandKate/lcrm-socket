7bcae1803a2058ea269c2a341769b4746210f3e8
Makarand Kate <makarand.kate@gmail.com>
2018-02-02 17:08:25 +0530

    log

--- index.js
+++ index.js
@@ -12,23 +12,27 @@ var activeAdmins={};
 io.on('connection', function(socket){
 	if(socket.handshake.query){
     if(socket.handshake.query.userType=="prospect"){
+      console.log("new prospect");
       activeUsers[socket.handshake.query.prospectId]={
         product:socket.handshake.query.product,
         socket:socket,
         timeStamp:new Date().getTime()
       };
+      console.log("informing all admins");
       for(key in activeAdmins){
         if(activeAdmins[key].product.indexOf(getActiveUsers[socket.handshake.query.prospectId].product)!=-1){
           activeAdmins[key].socket.emit('newUser',{"prospectId":socket.handshake.query.prospectId,"product":socket.handshake.query.product});
         }
       }  
     }else if(socket.handshake.query.userType=="admin"){
+      console.log("new admin");
       activeAdmins[socket.handshake.query.adminId]={
         socket:socket,
         product:socket.handshake.query.product,
         timeStamp:new Date().getTime()
       };
       var userList=[];
+      console.log("sending userList");
       for(key in activeUsers){
         if(activeAdmins[socket.handshake.query.adminId].product.indexOf(getActiveUsers[key].product)!=-1){
           userList.push({"prospectId":key,"product":getActiveUsers[key].product});
@@ -41,6 +45,7 @@ io.on('connection', function(socket){
   	socket.on('disconnect', function(){
       if(socket.handshake.query.userType=="prospect"){
         setTimeout(function(){
+          console.log("removing");
           for(key in activeAdmins){
             activeAdmins[key].socket.emit('userLeft',socket.handshake.query.prospectId)
           }
@@ -51,14 +56,9 @@ io.on('connection', function(socket){
       }
   	});
 
-    socket.on('getActiveUsers',function(msg){
-      console.log("getActiveUsers");
-      console.log(msg);
-      //activeAdmins[].socket.emit('userDetails',payload);
-    });
  
 });
-/*
+
 setInterval(function(){
 	var count=0;
 	for(key in activeUsers){
@@ -67,7 +67,7 @@ setInterval(function(){
 	console.log("active peers : "+count);
 
 },10000);
-*/
+
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:'+(process.env.PORT || 3000));
