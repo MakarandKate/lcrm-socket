1376af0eb4cbb2a1e3f6422d9cb7ec3e5b876e3c
Makarand Kate <makarand.kate@gmail.com>
2018-02-02 16:44:57 +0530

    final

--- index.js
+++ index.js
@@ -7,28 +7,67 @@ app.get('/', function(req, res){
 });
 
 var activeUsers={};
+var activeAdmins={};
 
 io.on('connection', function(socket){
-	if(socket.handshake.query.prospectId){
-    console.log("New user "+socket.handshake.query.prospectId)
-		activeUsers[socket.handshake.query.prospectId]={
-			socket:socket,
-			timeStamp:new Date().getTime()
-		};
+	if(socket.handshake.query){
+    if(socket.handshake.query.userType=="prospect"){
+      activeUsers[socket.handshake.query.prospectId]={
+        product:socket.handshake.query.product,
+        socket:socket,
+        timeStamp:new Date().getTime()
+      };
+      for(key in activeAdmins){
+        if(activeAdmins[key].product.indexOf(getActiveUsers[socket.handshake.query.prospectId].product)!=-1){
+          activeAdmins[key].socket.emit('newUser',{"prospectId":socket.handshake.query.prospectId,"product":socket.handshake.query.product});
+        }
+      }  
+    }else if(socket.handshake.query.userType=="admin"){
+      activeAdmins[socket.handshake.query.adminId]={
+        socket:socket,
+        product:socket.handshake.query.product,
+        timeStamp:new Date().getTime()
+      };
+      var userList=[];
+      for(key in activeUsers){
+        if(activeAdmins[socket.handshake.query.adminId].product.indexOf(getActiveUsers[key].product)!=-1){
+          userList.push({"prospectId":key,"product":getActiveUsers[key].product});
+        }
+      }
+      activeAdmins[socket.handshake.query.adminId].socket.emit('userList',userList);
+    }
+    
 	}
   	socket.on('disconnect', function(){
-  		delete activeUsers[socket.handshake.query.prospectId];
+      if(socket.handshake.query.userType=="prospect"){
+        setTimeout(function(){
+          for(key in activeAdmins){
+            activeAdmins[key].socket.emit('userLeft',socket.handshake.query.prospectId)
+          }
+          delete activeUsers[socket.handshake.query.prospectId];
+        },(5*60*1000));
+      }else if(socket.handshake.query.userType=="admin"){
+        delete activeAdmins[socket.handshake.query.adminId];
+      }
   	});
+
+    socket.on('getActiveUsers',function(msg){
+      console.log("getActiveUsers");
+      console.log(msg);
+      //activeAdmins[].socket.emit('userDetails',payload);
+    });
  
 });
+/*
 setInterval(function(){
 	var count=0;
-	for(key in activeQrs){
+	for(key in activeUsers){
 		++count;
 	}
 	console.log("active peers : "+count);
 
 },10000);
+*/
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:'+(process.env.PORT || 3000));
