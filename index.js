9148916fabc3a8beef9506e79e2096431ce0fdfa
Makarand Kate <makarand.kate@gmail.com>
2018-02-02 16:03:02 +0530

    boo

--- index.js
+++ index.js
@@ -6,30 +6,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var activeQrs={};
+var activeUsers={};
 
 io.on('connection', function(socket){
-	console.log("new connection :"+socket.id);
-	if(socket.handshake.query.sessionId){
-		activeQrs[socket.handshake.query.sessionId]={
+	if(socket.handshake.query.prospectId){
+    console.log("New user "+socket.handshake.query.prospectId)
+		activeUsers[socket.handshake.query.prospectId]={
 			socket:socket,
 			timeStamp:new Date().getTime()
 		};
 	}
   	socket.on('disconnect', function(){
-  		delete activeQrs[socket.handshake.query.sessionId];
+  		delete activeUsers[socket.handshake.query.prospectId];
   	});
-  socket.on('session', function(msg){
-    //console.log('New session: origin ' + msg.origin + '|'+ msg.sessionId);
-    
-  });
-  	socket.on('scan',function(msg){
-  		var payload=JSON.parse(msg);
-  		
-		console.log("scan :"+msg);
-  		activeQrs[payload.url].socket.emit('userDetails',payload);
-  		
-  	});
+ 
 });
 setInterval(function(){
 	var count=0;
